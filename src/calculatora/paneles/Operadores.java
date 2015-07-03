70b413e70527f13392491eda5c2fbf4eab450e2d
Jose <joseluisfp100@hotmail.com>
2015-07-03 09:45:52 +0200

    Eliminando warings

--- src/calculatora/paneles/Operadores.java
+++ src/calculatora/paneles/Operadores.java
@@ -7,9 +7,6 @@ import java.awt.GridLayout;
 
 import javax.swing.JButton;
 
-import calculatora.eventos.ControlFoco;
-import calculatora.eventos.ControlRaton;
-import calculatora.eventos.ControlTeclado;
 import calculatora.superClass.BloqueBotones;
 /**
  * Clase que pone operadores ("+", "-", "*" y "/") a los botones.
